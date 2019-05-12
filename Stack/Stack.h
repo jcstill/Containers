05b2b1d33a58ed5f41437174a5089856bf986950
jcstill <31301328+jcstill@users.noreply.github.com>
2019-05-12 11:14:33 -0700

    "19-05-12"

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -6,15 +6,14 @@ using namespace std;
 template<class T>struct node{
 	T data;												// Stores data
 	node<T> *next;										// Pointer to next node
-	node<T> *prev;										// Pointer to previous node
 };
 
 template<class T>class Stack{
 	private:
 		node<T> *first;									// Pointer to first node
-		node<T> *last;									// Pointer to last node
 		int count;										// Current size of the Stack
 		void copy(const Stack<T> &);					// Deep copy the Stack
+		// void hold();									// Create a holding stack 
 	public:
 		Stack();										// Default Constructor
 		Stack(int);										// Non-default Constructor
@@ -28,7 +27,7 @@ template<class T>class Stack{
 		void pop();										// Delete first element
 		void swap(int, int);							// Swap elements
 		
-		void resize(int);								// Make Stack X elements long (shortening Stack will destroy remaining elements)
+		void resize(int);								// Make Stack X elements long adding or removing at the top
 		void erase(int);								// Erase element at index
 		void clear(int, int);							// Erase content range (first, last)
 		void insert(const T&, int);						// Insert element at position
@@ -52,5 +51,111 @@ Functions for iterator:
 	get_allocator	Get allocator (public member function )
 */
 };
+
+template<class T>Stack<T>::Stack(){
+	first = nullptr;
+	count = 0;
+}
+template<class T>Stack<T>::Stack(int x){
+	first = nullptr;
+	count = 0;
+	T tmp{};
+	for(int i=0; i<x; i++){
+		push(tmp);
+	}
+}
+template<class T>Stack<T>::Stack(const Stack<T>& x){
+	first = nullptr;
+	count = 0;
+	copy(x);
+}
+template<class T>Stack<T>::~Stack(){
+	node<T> *tmp;
+	while(first != nullptr){
+		tmp = first;
+		first = first -> next;
+		delete tmp;
+	}
+	count = 0;
+}
+template<class T>bool Stack<T>::isEmpty(){
+	return count == 0;
+}
+template<class T>int Stack<T>::sizeOf() const{
+	return count;
+}
+template<class T>const T& Stack<T>::top() const{
+	return first -> data;
+}
+
+
+
+
+template<class T>void Stack<T>::push(const T& x){
+	node<T> *current = nullptr;
+	node<T> *curback = nullptr;
+	node<T> *newnode = nullptr;
+	newnode = new node<T>;
+	newnode -> data = x;
+	newnode -> next = nullptr;
+	newnode -> prev = nullptr;
+	if(first == nullptr){
+		first = newnode;
+		last = newnode;
+		count++;
+	}else{
+		current = first;
+		if(y == 0){
+			first -> prev = newnode;
+			newnode -> next = first;
+			first = newnode;
+			count++;
+		}else if(y <= count){
+			for(int i=0; i<y; i++){
+				curback = current;
+				current = current -> next;
+			}
+			if(current != nullptr){
+				current -> prev = newnode;
+				newnode -> next = current;
+				newnode -> prev = curback;
+				curback -> next = newnode;
+			}else{
+				curback -> next = newnode;
+				newnode -> prev = curback;
+				last = newnode;
+			}
+			count++;
+		}
+	}
+}
+template<class T>void Stack<T>::pop(){
+	node<T> *current = first;
+	node<T> *curback = nullptr;
+	if(x == 0){
+		first = first -> next;
+		if(first != nullptr){
+			first -> prev = nullptr;
+		}else{
+			last = nullptr;
+		}
+		delete current;
+		count--;
+	}else if(x<count){
+		for(int i=0; i<x; i++){
+			current = current -> next;
+		}
+		curback = current -> prev;
+		curback -> next = current -> next;
+		if(current -> next != nullptr){
+			current -> next -> prev = curback;
+		}
+		if(current == last){
+			last = curback;
+		}
+		count--;
+		delete current;
+	}
+}
 
 #endif
