c7c70dded3c5e084df0d1c006a5c5092f6eba8ee
jcstill <jacobcstill@gmail.com>
2019-11-02 00:14:27 -0700

    Update 11-02-2019 12:14AM

--- String/String.h
+++ String/String.h
@@ -6,61 +6,117 @@ using namespace std;
 
 class String{
 	private:
-		int size;
-		int capacity;
-		List<char> str;
-		void copy(const String&);						// Deep copy the String
+		int size;										// Current size of the String
+		int capacity;									// Total capacity of the String
+		List<char> str;									// Actual String stored as a list
+		char* array;
+		
+		void copy(const String&);			//FIXME		// Deep copy the String
 	public:
-		String();
-		String(int);
-		String(const String&);
-		~String();
-		
-		int sizeOf() const;
-		int length() const;
-		void resize(int);
-		int capacityOf() const;
-		void reserve(int);
-		void clear();
-		bool isEmpty();
-		void shrink_to_fit();
-		
-		char at(int x) const;
-		
-		void pop_back();
-		void push_back(const char);
-		
-		void print();
+		String();										// Default Constructor
+		String(int);									// Non-default Constructor
+		String(const String&);				//FIXME		// Copy Constructor
+		~String();										// Destructor
+		// Capacity and sizing functions
+		int sizeOf() const;								// Returns size of the String
+		int length() const;								// Returns size of the String
+		void resize(int);								// Resize String - Can modify String
+		int capacityOf() const;							// Returns the size of allocated storage
+		void reserve(int);								// Resize String - Can't modify String
+		void clear();									// Clear the String - Resets capacity to 0
+		bool isEmpty();									// Check if String is empty
+		void shrink_to_fit();							// Shrink capacity to fit size
+		// Element access
+		char operator[](int) const;						// Return character in String
+		char at(int) const;								// Return character in String
+		char back() const;								// Return last character
+		char front() const;								// Return first character
+		// Modifiers
+		String& operator+=(const String&);				// Append String to end of this String
+		String& operator+=(const char*);				// Append const char* to end of this String - ei.( "Hello World" )
+		String& operator+=(char);						// Append char to end of this String - ei.( 'Y' )
+		String& append(const String&);					// Append String to end of this String
+		String& append(const char*);					// Append const char* to end of this String - ei.( "Hello World" )
+		String& append(char);							// Append char to end of this String - ei.( 'Y' )
+		void push_back(const char);						// Append character to String
+		String& assign(const String&);					// Replaces this String with String passed in
+		String& assign(const char*);					// Replaces this String with const char* passed in - ei.( "Hello World" )
+		String& assign(char);							// Replaces this String with char passed in - ei.( 'Y' )
+		String& insert(int, const String&);				// Inserts a copy of String into this String
+		String& insert(int, const char*);				// Inserts a copy of const char* into this String - ei.( "Hello World" )
+		String& insert(int, char);						// Inserts a copy of char into this String - ei.( 'Y' )
+		void erase(int, int);							// Erase portion of string ()
+		String& replace(int, int, const String&);		// Replace portion of this String with String
+		String& replace(int, int, const char*);			// Replace portion of this String with const char* - ei.( "Hello World" )
+		String& replace(int, int, char);				// Replace portion of this String with char - ei.( 'Y' )
+		void swap(String&);								// Swaps contents of this String with String
+		void pop_back();								// Delete last character
+		// String Operations
+		// const char* c_str();						// Return pointer to char array of String
+		// const char* data() const;						//
+
+
+
+
+// get_allocator
+// copy
+// find
+// rfind
+// find_first_of
+// find_last_of
+// find_first_not_of
+// find_last_not_of
+// substr
+// compare
+
+
+
+
+
+
+
+
+
+
+		void print();									// 
 };
 
 String::String(){
 	List<char> str;
 	size = 0;
 	capacity = 0;
+	// array = NULL;
 }
 String::String(int x){
 	List<char> str(x);
 	size = x;
 	capacity = x;
+	// array = NULL;
 }
 String::String(const String& x){
 	List<char> str;
 	size = 0;
 	capacity = 0;
+	// array = NULL;
 	copy(x);
 }
 String::~String(){
 	str.~List();
 	size = 0;
 	capacity = 0;
+	// if(array != NULL){
+		// cout << "yeet\n";
+		// delete [] array;
+	// }
 }
 void String::copy(const String& x){
 	for(int i=0; i<x.sizeOf(); i++){
-		str.push_front(x.at(i));
+		str.push_back(x.at(i));
 		size++;
 		capacity++;
 	}
 }
+// Capacity and sizing functions
 void String::pop_back(){
 	str.pop_back();
 	size--;
@@ -120,34 +176,189 @@ void String::shrink_to_fit(){
 	}
 	capacity = size;
 }
+// Element access
+char String::operator[](int x) const{
+	return str.retAt(x);
+}
 char String::at(int x) const{
 	return str.retAt(x);
 }
+char String::back() const{
+	return str.retEnd();
+}
+char String::front() const{
+	return str.retFront();
+}
+// Modifiers
+String& String::operator+=(const String& x){
+	for(int i=0; i<x.sizeOf(); i++){
+		str.push_back(x.at(i));
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::operator+=(const char* x){
+	int constsize = 0;
+	while(x[constsize] != '\0'){
+		constsize++;
+	}
+	for(int i=0; i<constsize; i++){
+		str.push_back(x[i]);
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::operator+=(char x){
+	str.push_back(x);
+	size++;
+	capacity++;
+	return *this;
+}
+String& String::append(const String& x){
+	for(int i=0; i<x.sizeOf(); i++){
+		str.push_back(x.at(i));
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::append(const char* x){
+	int constsize = 0;
+	while(x[constsize] != '\0'){
+		constsize++;
+	}
+	for(int i=0; i<constsize; i++){
+		str.push_back(x[i]);
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::append(char x){
+	str.push_back(x);
+	size++;
+	capacity++;
+	return *this;
+}
+String& String::assign(const String& x){
+	clear();
+	for(int i=0; i<x.sizeOf(); i++){
+		str.push_back(x.at(i));
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::assign(const char* x){
+	clear();
+	int constsize = 0;
+	while(x[constsize] != '\0'){
+		constsize++;
+	}
+	for(int i=0; i<constsize; i++){
+		str.push_back(x[i]);
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::assign(char x){
+	clear();
+	str.push_back(x);
+	size++;
+	capacity++;
+	return *this;
+}
+String& String::insert(int y, const String& x){
+	for(int i=x.sizeOf()-1; i>=0; i--){
+		str.insert(x.at(i), y);
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::insert(int y, const char* x){
+	int constsize = 0;
+	while(x[constsize] != '\0'){
+		constsize++;
+	}
+	for(int i=constsize-1; i>=0; i--){
+		str.insert(x[i], y);
+		size++;
+		capacity++;
+	}
+	return *this;
+}
+String& String::insert(int y, char x){
+	str.insert(x, y);
+	size++;
+	capacity++;
+	return *this;
+}
+void String::String::erase(int x, int y){
+	int tmp = 0;
+	if(x > y){
+		tmp = x;
+		x = y;
+		y = tmp;
+	}
+	str.clear(x, y);
+	tmp = y - x +1;
+	size -= tmp;
+	capacity -= tmp;
+}
+String& String::replace(int y, int len, const String& x){
+	erase(y, y+len);
+	insert(y, x);
+	return *this;
+}
+String& String::replace(int y, int len, const char* x){
+	erase(y, y+len);
+	insert(y, x);
+	return *this;
+}
+String& String::replace(int y, int len, char x){
+	erase(y, y+len);
+	insert(y, x);
+	return *this;
+}
+void String::swap(String& x){
+	String tmp(x);
+	x.assign(*this);
+	assign(tmp);
+	tmp.~String();
+}
+// String Operations
+// const char* String::c_str(){
+// 	delete [] array;
+// 	array = new char[size+1];
+// 	for(int i=0; i<size; i++){
+// 		array[i] = at(i);
+// 	}
+// 	array[size] = '\0';
+// 	return array;
+// }
 
-// String::append
-// String::assign
-// String::back
-// String::capacity
-// String::compare
-// String::copy
-// String::c_str
-// String::data
-// String::erase
-// String::find
-// String::find_first_not_of
-// String::find_first_of
-// String::find_last_not_of
-// String::find_last_of
-// String::front
-// String::get_allocator
-// String::insert
-// String::operator+=
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // String::operator=
-// String::operator[]
-// String::replace
-// String::rfind
-// String::substr
-// String::swap
 
 void String::print(){
 	str.print();
--- String/driver.cpp
+++ String/driver.cpp
@@ -70,7 +70,6 @@ int main(){
 	cout << "capacityOf" << '\n';
 	cout << tmp1.capacityOf() << "\n\n";
 	
-	
 	cout << "clear" << '\n';
 	tmp1.clear();
 	tmp1.print();
@@ -83,7 +82,7 @@ int main(){
 	
 	cout << "push_back" << '\n';
 	for(char i=0; i<6; i++){
-		tmp1.push_back(i+65);
+		tmp1.push_back(i+48);
 	}
 	tmp1.print();
 	cout << '\n';
@@ -124,7 +123,200 @@ int main(){
 	cout << "capacityOf" << '\n';
 	cout << tmp3.capacityOf() << "\n\n";
 	
-	
+	cout << "[1]" << '\n';
+	cout << tmp3[1] << "\n\n";
+
+	cout << "at 1" << '\n';
+	cout << tmp3.at(1) << "\n\n";
+
+	cout << "back" << '\n';
+	cout << tmp3.back() << "\n\n";
+
+	cout << "front" << '\n';
+	cout << tmp3.front() << "\n\n";
+
+	cout << "+= tmp1" << '\n';
+	tmp3 += tmp1;
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "+= \'Y\'" << '\n';
+	tmp3 += 'Y';
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "+= \"YEET\"" << '\n';
+	tmp3 += "YEET";
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "append tmp1" << '\n';
+	tmp3.append(tmp1);
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "append \'y\'" << '\n';
+	tmp3.append('y');
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "append \"yeet\"" << '\n';
+	tmp3.append("yeet");
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "assign tmp1" << '\n';
+	tmp3.assign(tmp1);
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "assign \'y\'" << '\n';
+	tmp3.assign('y');
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "assign \"yeet\"" << '\n';
+	tmp3.assign("yeet");
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "insert tmp1 at 1" << '\n';
+	tmp3.insert(1, tmp1);
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "insert \'y\' at 2" << '\n';
+	tmp3.insert(2,'y');
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "insert \"hello\" at 3" << '\n';
+	tmp3.insert(3,"hello");
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "erase 2-4" << '\n';
+	tmp3.erase(4, 2);
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "replace 1-3 with tmp1" << '\n';
+	tmp3.replace(1, 2, tmp1);
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "replace 1-3 with \'G\'" << '\n';
+	tmp3.replace(1, 2, 'G');
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "replace 1-3 with \'GOOFY\'" << '\n';
+	tmp3.replace(1, 2, "GOOFY");
+	tmp3.print();
+	cout << "sizeOf" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf" << '\n';
+	cout << tmp3.capacityOf() << "\n\n";
+
+	cout << "swap tmp3 and tmp1" << '\n';
+	cout << "tmp3:\n";
+	tmp3.swap(tmp1);
+	tmp3.print();
+	cout << "sizeOf tmp3" << '\n';
+	cout << tmp3.sizeOf() << "\n";
+	cout << "length of tmp3" << '\n';
+	cout << tmp3.length() << "\n";
+	cout << "capacityOf tmp3" << '\n';
+	cout << tmp3.capacityOf() << "\ntmp1:\n";
+	tmp1.print();
+	cout << "sizeOf tmp1" << '\n';
+	cout << tmp1.sizeOf() << "\n";
+	cout << "length of tmp1" << '\n';
+	cout << tmp1.length() << "\n";
+	cout << "capacityOf tmp1" << '\n';
+	cout << tmp1.capacityOf() << "\n\n";
+
+	// cout << "c_str of tmp3" << '\n';
+	// cout << tmp3.c_str() << "\n\n";
+
+
 	tmp1.~String();
 	tmp2.~String();
 	tmp3.~String();
